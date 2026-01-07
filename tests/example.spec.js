84d5d1879ef4e11a5576f7e58f9c0b18561d480d
SadkiMohammed <sadki123sadki@outlook.fr>
2026-01-07 11:29:22 +0100

    TC_4

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,17 +10,21 @@ test.describe('Automatisation Exercises', () => {
     await page.getByRole('button', { name: /accept all|tout accepter|consent/i }).click();
   });
 
-    test('créer compte et le supprimer', async ({ page }) => {
+    test('Test Case 1', async ({ page }) => {
       await tools.CreateAcountAndDelete(page);
     });
 
-    test('se connecter et supprimer le compte', async ({ page }) => {
+    test('Test Case 2', async ({ page }) => {
       await tools.CreateAcountAndLogout(page);
       await tools.Login(page);
       await tools.DeleteAccount(page);
     });
 
-    test('se connecter avec erreur', async ({ page }) => {
+    test('Test Case 3', async ({ page }) => {
       await tools.LoginWithError(page);
+    });
+
+    test('Test Case 4', async ({ page }) => {
+      await tools.CreateAcountAndLogout(page);
     });
 });
