42a1d530c4e208bf065a58a3867ace215544ddd4
SadkiMohammed <sadki123sadki@outlook.fr>
2026-01-08 14:27:36 +0100

    maj

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,7 +10,7 @@ test.describe('Automatisation Exercises', () => {
     await page.getByRole('button', { name: /accept all|tout accepter|consent/i }).click();
   });
 
-    /*test('Test Case 1', async ({ page }) => {
+    test('Test Case 1', async ({ page }) => {
       await tools.CreateAcountAndDelete(page);
     });
 
@@ -35,7 +35,7 @@ test.describe('Automatisation Exercises', () => {
 
     test('Test Case 6', async ({ page }) => {
       await tools.ContactUs(page);
-    });*/
+    });
 
     test('Test Case 7', async ({ page }) => {
       await tools.TestCases(page);
